bf841c35044004ee74aef18bcfc15d6e4963d84e
Koroshh <mateuszniema1@gmail.com>
2021-04-13 20:04:46 +0200

    ez

--- commands/music/play.js
+++ commands/music/play.js
@@ -34,7 +34,7 @@ module.exports = {
 		};
     const embed4 = new Discord.MessageEmbed()
 	.setColor('#0099ff')
-	.addField(`🎶 ▸ `, `**[${song.title}]**``**(${song.url})`)	
+	.addField("🎶 ▸ Dodano do kolejki", `[${song.title}](${song.url})`)	
 	if (serverQueue) {
 			serverQueue.songs.push(song);
 			console.log(serverQueue.songs);
