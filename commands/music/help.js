57d44b3010eb678b7ce3d8079168b4a8872431e6
Koroshh <mateuszniema1@gmail.com>
2021-04-14 12:39:32 +0200

    ez

--- commands/music/help.js
+++ commands/music/help.js
@@ -10,8 +10,8 @@ module.exports = {
 	  .setColor('#0099ff')
   	.setTitle('Alts')
   	.setAuthor('Pomoc', 'https://imgur.com/pmx6yfY.png')
-  	.addField('Alts to najłatwiejszy sposób na odtwarzanie muzyki na serwerze Discord. Jak narazie współpracuje z YouTube.' ,false)
-    .addField('Aby rozpocząć, dołącz do kanału głosowego i -play piosenkę. Możesz używać nazw utworów, linków do filmów i linków do playlist.', false)
+  	.addField('Alts to najłatwiejszy sposób na odtwarzanie muzyki na serwerze Discord. Jak narazie współpracuje z YouTube.' ,)
+    .addField('Aby rozpocząć, dołącz do kanału głosowego i -play piosenkę. Możesz używać nazw utworów, linków do filmów i linków do playlist.', )
 	  .setFooter('Autorem bota jest blaszkaaa#8802', 'https://imgur.com/pmx6yfY.png');
 
       message.channel.send(help)
