5178b4d2a0be7f0577c6e9a125ecbec23a58debf
Koroshh <mateuszniema1@gmail.com>
2021-04-14 12:34:31 +0200

    ez

--- commands/music/help.js
+++ commands/music/help.js
@@ -8,11 +8,10 @@ module.exports = {
 
     const help = new Discord.MessageEmbed()
 	  .setColor('#0099ff')
-  	.setTitle('FanatykPapieża')
-  	.setURL('https://discord.js.org/')
-  	.setAuthor('Pomoc', 'https://imgur.com/pmx6yfY.png',      'https://discord.js.org')
-  	.addField('Alts to najłatwiejszy sposób na odtwarzanie muzyki na serwerze Discord. Jak narazie współpracuje z YouTube.')
-    .addField('Aby rozpocząć, dołącz do kanału głosowego i -play piosenkę. Możesz używać nazw utworów, linków do filmów i linków do playlist.')
+  	.setTitle('Alts')
+  	.setAuthor('Pomoc', 'https://imgur.com/pmx6yfY.png')
+  	.addField('Alts to najłatwiejszy sposób na odtwarzanie muzyki na serwerze Discord. Jak narazie współpracuje z YouTube.' ,false)
+    .addField('Aby rozpocząć, dołącz do kanału głosowego i -play piosenkę. Możesz używać nazw utworów, linków do filmów i linków do playlist.', false)
 	  .setFooter('Autorem bota jest blaszkaaa#8802', 'https://imgur.com/pmx6yfY.png');
 
       message.channel.send(help)
