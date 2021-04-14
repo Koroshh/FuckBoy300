02e62fc7981e09ebc2faf1f993b3a158c80c473c
Koroshh <mateuszniema1@gmail.com>
2021-04-14 12:30:36 +0200

    ez

--- commands/music/help.js
+++ commands/music/help.js
@@ -11,7 +11,8 @@ module.exports = {
   	.setTitle('FanatykPapieża')
   	.setURL('https://discord.js.org/')
   	.setAuthor('Pomoc', 'https://imgur.com/pmx6yfY.png',      'https://discord.js.org')
-  	.setDescription('Liste komend znajdziesz pod $commends')
+  	.addField('Alts to najłatwiejszy sposób na odtwarzanie muzyki na serwerze Discord. Jak narazie współpracuje z YouTube.')
+    .addField('Aby rozpocząć, dołącz do kanału głosowego i -play piosenkę. Możesz używać nazw utworów, linków do filmów i linków do playlist.')
 	  .setFooter('Autorem bota jest blaszkaaa#8802', 'https://imgur.com/pmx6yfY.png');
 
       message.channel.send(help)
