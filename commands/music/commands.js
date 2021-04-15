eba238b6119705ec70a4099cca96e12c2b551a32
Koroshh <mateuszniema1@gmail.com>
2021-04-16 00:52:50 +0200

    ez

--- commands/music/commands.js
+++ commands/music/commands.js
@@ -9,8 +9,7 @@ module.exports = {
     const help = new Discord.MessageEmbed()
 	  .setColor('#0099ff')
   	.setTitle('Komendy')
-  	.setURL('https://imgur.com/rZTaCPx.png')
-  	.setAuthor('Komendy', 'https://imgur.com/rZTaCPx.png','https://imgur.com/rZTaCPx.png')
+  	.setAuthor('Alts', 'https://imgur.com/rZTaCPx.png',)
   	.setDescription('Moje komendy możesz sprawdzić pod tym linkiem.')
 	  .setFooter('Autorem bota jest blaszkaaa#8802', 'https://imgur.com/rZTaCPx.png');
 
