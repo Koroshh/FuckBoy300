1e9e5ff924bae805dd2c3ac11f7fb0be313c13ad
Koroshh <mateuszniema1@gmail.com>
2021-04-15 18:41:47 +0200

    hft

--- commands/music/play.js
+++ commands/music/play.js
@@ -12,14 +12,9 @@ module.exports = {
     description: 'Advanced music bot',
     run: async(message,args, cmd, client, Discord) =>{
 
-
+		
         //Checking for the voicechannel and permissions (you can add more permissions if you like).
-        const voice_channel = message.member.voice.channel;
-        if (!voice_channel) return message.channel.send('You need to be in a channel to execute this command!');
-        const permissions = voice_channel.permissionsFor(message.client.user);
-        if (!permissions.has('CONNECT')) return message.channel.send('You dont have the correct permissins');
-        if (!permissions.has('SPEAK')) return message.channel.send('You dont have the correct permissins');
-
+       
         //This is our server queue. We are getting this server queue from the global queue.
         const server_queue = queue.get(message.guild.id);
 
