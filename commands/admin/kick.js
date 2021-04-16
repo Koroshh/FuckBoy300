1efb4afdf13404b293218d6e22c8a40156af4890
Koroshh <mateuszniema1@gmail.com>
2021-04-16 15:37:16 +0200

    ez

--- commands/admin/kick.js
+++ commands/admin/kick.js
@@ -5,9 +5,17 @@ module.exports = {
   usage: "kick",
   description: "kick user",
   run: async (client, message, member, args) => {
-    if (message.content == '$kick'){
-      if (member.hasPermission('KICK_MEMBERS')) {
-        console.log('This member can kick');
-    }};
+    
+    if (msg.member.hasPermission("KICK_MEMBERS")) {
+      if (msg.members.mentions.first()) {
+          try {
+              msg.members.mentions.first().kick();
+          } catch {
+              msg.reply("I do not have permissions to kick " + msg.members.mentions.first());
+          }
+      } else {
+          msg.reply("You do not have permissions to kick " + msg.members.mentions.first());
+      }
+  }
 
 }};
