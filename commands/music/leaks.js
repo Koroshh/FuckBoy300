0fc92d449377c2645b7af35491cbfcd2bedfcdcb
Koroshh <mateuszniema1@gmail.com>
2021-04-17 01:31:23 +0200

    ez

--- commands/music/leaks.js
+++ commands/music/leaks.js
@@ -6,7 +6,7 @@ module.exports = {
   description: "vpn account",
   run: async (client, message, args) => {
 
-    var konta = newArray["1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20"];
+    var konta = new Array["1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20"];
     var konto = Math.ceil(Math.random()*konta.length);
 
     if (message.content === '$leaks') {
