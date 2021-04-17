956b57ee0125637f98b8ac1edfd43e68eb35a996
Koroshh <mateuszniema1@gmail.com>
2021-04-17 18:09:37 +0200

    ez

--- commands/music/leaks.js
+++ commands/music/leaks.js
@@ -6,15 +6,16 @@ module.exports = {
   description: "vpn account",
   run: async (client, message, args) => {
 
-    var kolory = ['bialy','zielony','niebieski','czerwony','pomaranczowy'];
+    var konta = ['bialy','zielony','niebieski','czerwony','pomaranczowy'];
 
     function rand(range_from,range_to){
 	    var r = Math.random();
 	    r = r * (range_to-range_from) + range_from;
       return Math.round(r);
   };
-  var wylosowany_kolor = kolory[rand(0,kolory.length-1)];
+  var wylosowane_konto = konta[rand(0,konta.length-1)];
   if(message.content == '$leaks'){
-    message.channel.send(wylosowany_kolor);
+    message.channel.send(wylosowane_konto);
+    message.react('😎');
   };
   }};
