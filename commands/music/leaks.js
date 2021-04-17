04b8c64a5a135f338ecb4af666dceedff737b75c
Koroshh <mateuszniema1@gmail.com>
2021-04-17 19:27:20 +0200

    ez

--- commands/music/leaks.js
+++ commands/music/leaks.js
@@ -4,7 +4,7 @@ module.exports = {
   aliases: ["le"],
   usage: "leaks",
   description: "vpn account",
-  run: async (client, message, args) => {
+  run: async (client, cmd, member, message, args) => {
 
     var konta = ['bialy','zielony','niebieski','czerwony','pomaranczowy'];
 
@@ -12,10 +12,32 @@ module.exports = {
 	    var r = Math.random();
 	    r = r * (range_to-range_from) + range_from;
       return Math.round(r);
-  };
-  var wylosowane_konto = konta[rand(0,konta.length-1)];
-  if(message.content == '$leaks'){
-    message.channel.send(wylosowane_konto);
-    message.react('😎');
-  };
+    };
+    
+    var wylosowane_konto = konta[rand(0,konta.length-1)];
+    
+    if(message.content == '$leaks'){
+      message.channel.send(wylosowane_konto);
+      message.react('😎');
+    };
+
+    if (cmd === `$dm`) {
+      let dUser =
+       message.guild.member(message.mentions.users.first()) ||
+       message.guild.members.get(args[0]);
+      if (!dUser) return message.channel.send("Can't find user!");
+      if (!message.member.hasPermission('ADMINISTRATOR'))
+       return message.reply("You can't you that command!");
+      let dMessage = args.join(' ').slice(22);
+      if (dMessage.length < 1) return message.reply('You must supply a message!');
+     
+      dUser.send(`${dUser} A moderator from WP Coding Club sent you: ${dMessage}`);
+     
+      message.author.send(
+       `${message.author} You have sent your message to ${dUser}`
+      );
+     }
+
+
+
   }};
