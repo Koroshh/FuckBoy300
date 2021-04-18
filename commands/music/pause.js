5afe574a544b6d54f0847546b5a51105936a9e28
Koroshh <mateuszniema1@gmail.com>
2021-04-18 14:41:38 +0200

    ez

--- commands/music/pause.js
+++ commands/music/pause.js
@@ -7,11 +7,13 @@ module.exports = {
   run: async (client, message, args) => {
 
     if (message.content === '$pause') {
-      const Embed = new RichEmbed()
-              .setTitle("Helper Embed")
-              .setColor(0xFF0000)
-              .setDescription("Make sure to use the !help to get access to the commands");
-      message.author.send(Embed);
+      const help = new Discord.MessageEmbed()
+	  .setColor('#0099ff')
+  	.setTitle('Pomoc')
+  	.setAuthor('Alts', 'https://imgur.com/rZTaCPx.png')
+    .setDescription('Alts to najłatwiejszy sposób na odtwarzanie muzyki na serwerze Discord. Jak narazie współpracuje z YouTube. Aby rozpocząć, dołącz do kanału głosowego i $play <link>. Możesz używać inków do piosenek. Chcesz sprawdzić jakie mam komendy? Polecam $commands.' )
+	  .setFooter('Autorem bota jest blaszkaaa#8802', 'https://imgur.com/rZTaCPx.png');
+      message.author.send(help);
     }
 
 		const serverQueue = message.client.queue.get(message.guild.id);
