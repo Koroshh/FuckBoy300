a5a7725f5c1732d1e807353b997534a58f4a7e6a
Koroshh <mateuszniema1@gmail.com>
2021-04-18 15:49:10 +0200

    ez

--- commands/music/leaks.js
+++ commands/music/leaks.js
@@ -7,11 +7,19 @@ module.exports = {
   run: async (client, message, args) => {
 
     var konta = [
-    'bialy',
-    'zielony',
-    'niebieski',
-    'czerwony',
-    'pomaranczowy',
+    'williamjbradford@yahoo.com:Namleps827 | Expiry = 2023-03-04 |',
+    'ame68@msn.com:Acme1968 | Expiry = 2024-02-17 |',
+    'paradoxy@telus.net:enigma666 | Expiry = 2023-07-07 |',
+    'imironvasile@yahoo.com:miron2112 | Expiry = 2023-03-02 |',
+    'ztniles@gmail.com:Hugh514! | Expiry = 2022-12-01 |',
+    'tonyblayk@gmail.com:zinny123 | Expiry = 2022-12-01 |',
+    'tokolight629@gmail.com:698543210aA | Expiry = 2022-10-10 |',
+    'nathanmontone@gmail.com:Wintbr!100 | Expiry = 2022-07-04 |',
+    'taffjensen@gmail.com:Orion528 | Expiry = 2022-05-11 |',
+    'ahmed.alali@gmail.com:Valbuena1 | Expiry = 2022-03-04 |',
+    'kl.20@hotmail.com:smile123 | Expiry = 2022-02-26 |',
+    'alisonangst@icloud.com:Den22Dzksk | Expiry = 2022-02-11 |',
+    'raiders.rock36@hotmail.com:Cougar#23 | Expiry = 2022-01-18 |',
     ];
 
     function rand(range_from,range_to){
